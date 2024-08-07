1b92dd96d08b94d9bfcdd98ef9fbb0068aeb0477
Ares Stavropoulos <aresstav04@gmail.com>
2024-08-07 00:46:58 -0500

    add astronvim go to references bindings

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
   -- import/override with your plugins folder
 }
--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,67 +1,67 @@
 -- Define our custom mappings
 local custom_mappings = {
   n = {
-    ["grr"] = {
-      function()
-        local function lsp_reference_float()
-          vim.lsp.handlers["textDocument/references"] = function(_, result, ctx)
-            if not result or vim.tbl_isempty(result) then
-              vim.notify("No references found")
-              return
-            end
-            local bufnr = vim.api.nvim_create_buf(false, true)
-            local contents = {}
-            local references = {}
-            for i, ref in ipairs(result) do
-              local fname = vim.uri_to_fname(ref.uri)
-              local line = ref.range.start.line + 1
-              local text = vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1] or ""
-              table.insert(contents, string.format("%s:%d", fname, line))
-              table.insert(contents, "   " .. text)
-              table.insert(references, {uri = ref.uri, range = ref.range})
-            end
-            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
-            vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
-            vim.api.nvim_buf_set_option(bufnr, 'filetype', 'lsp-references')
-            -- Set up keymaps for the floating window
-            local function jump_to_reference()
-              local cursor = vim.api.nvim_win_get_cursor(0)
-              local index = math.floor(cursor[1] / 3) + 1
-              if references[index] then
-                vim.api.nvim_win_close(0, true)
-                vim.lsp.util.jump_to_location(references[index], "utf-8")
-              end
-            end
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CR>', '', {
-              callback = jump_to_reference,
-              noremap = true,
-              silent = true,
-            })
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', 'q', '', {
-              callback = function() vim.api.nvim_win_close(0, true) end,
-              noremap = true,
-              silent = true,
-            })
-            local width = 80
-            local height = math.min(#contents, 10)
-            local opts = {
-              relative = 'cursor',
-              width = width,
-              height = height,
-              col = 0,
-              row = 1,
-              style = 'minimal',
-              border = 'rounded'
-            }
-            local win = vim.api.nvim_open_win(bufnr, true, opts)
-            vim.api.nvim_win_set_option(win, 'cursorline', true)
-          end
-        end
-        lsp_reference_float()
-        vim.lsp.buf.references()
-      end,
-      desc = "Go to references (interactive floating window)"
-    },
+    -- ["grr"] = {
+    --   function()
+    --     local function lsp_reference_float()
+    --       vim.lsp.handlers["textDocument/references"] = function(_, result, ctx)
+    --         if not result or vim.tbl_isempty(result) then
+    --           vim.notify("No references found")
+    --           return
+    --         end
+    --         local bufnr = vim.api.nvim_create_buf(false, true)
+    --         local contents = {}
+    --         local references = {}
+    --         for i, ref in ipairs(result) do
+    --           local fname = vim.uri_to_fname(ref.uri)
+    --           local line = ref.range.start.line + 1
+    --           local text = vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1] or ""
+    --           table.insert(contents, string.format("%s:%d", fname, line))
+    --           table.insert(contents, "   " .. text)
+    --           table.insert(references, {uri = ref.uri, range = ref.range})
+    --         end
+    --         vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+    --         vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+    --         vim.api.nvim_buf_set_option(bufnr, 'filetype', 'lsp-references')
+    --         -- Set up keymaps for the floating window
+    --         local function jump_to_reference()
+    --           local cursor = vim.api.nvim_win_get_cursor(0)
+    --           local index = math.floor(cursor[1] / 3) + 1
+    --           if references[index] then
+    --             vim.api.nvim_win_close(0, true)
+    --             vim.lsp.util.jump_to_location(references[index], "utf-8")
+    --           end
+    --         end
+    --         vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CR>', '', {
+    --           callback = jump_to_reference,
+    --           noremap = true,
+    --           silent = true,
+    --         })
+    --         vim.api.nvim_buf_set_keymap(bufnr, 'n', 'q', '', {
+    --           callback = function() vim.api.nvim_win_close(0, true) end,
+    --           noremap = true,
+    --           silent = true,
+    --         })
+    --         local width = 80
+    --         local height = math.min(#contents, 10)
+    --         local opts = {
+    --           relative = 'cursor',
+    --           width = width,
+    --           height = height,
+    --           col = 0,
+    --           row = 1,
+    --           style = 'minimal',
+    --           border = 'rounded'
+    --         }
+    --         local win = vim.api.nvim_open_win(bufnr, true, opts)
+    --         vim.api.nvim_win_set_option(win, 'cursorline', true)
+    --       end
+    --     end
+    --     lsp_reference_float()
+    --     vim.lsp.buf.references()
+    --   end,
+    --   desc = "Go to references (interactive floating window)"
+    -- },
 ["ga"] = {
       function()
         
