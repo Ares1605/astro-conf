0bed4e70575bd2085d60300b9d5c79ff90610b98
Ares Stavropoulos <aresstav04@gmail.com>
2024-09-20 03:07:10 -0500

    add colortheme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -23,22 +23,22 @@
 --     end,
 --     name='ares', priority=1000},
 -- }
-return {
-  'luisiacc/gruvbox-baby',
-  branch = 'main',
-  config = function()
-    vim.cmd[[colorscheme gruvbox-baby]]
-  end
-}
-
 -- return {
---     'sainnhe/gruvbox-material',
---     lazy = false,
---     priority = 1000,
---     config = function()
---       -- Optionally configure and load the colorscheme
---       -- directly inside the plugin declaration.
---       vim.g.gruvbox_material_enable_italic = true
---       vim.cmd.colorscheme('gruvbox-material')
---     end
---   }
+--   'luisiacc/gruvbox-baby',
+--   branch = 'main',
+--   config = function()
+--     vim.cmd[[colorscheme gruvbox-baby]]
+--   end
+-- }
+
+return {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  }
