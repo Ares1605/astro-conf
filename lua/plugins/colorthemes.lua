04ccd3b58f162de771207101bc9cf96f4ebc5349
Ares Stavropoulos <aresstav04@gmail.com>
2024-12-18 23:36:25 -0600

    add themery plugin

---
+++ lua/plugins/colorthemes.lua
@@ -0,0 +1,100 @@
+return {
+  {
+    "ilof2/posterpole.nvim",
+    priority=1000,
+    lazy=false
+  },
+  {
+    "sample-usr/rakis.nvim",
+    priority=1000,
+    lazy=false
+  },
+  {
+    'Donearm/Ubaryd',
+    priority=1000,
+    lazy = false
+  },
+  {
+    'coffee-theme/coffee.vim',
+    priority = 1000,
+    lazy=false
+  },
+  {
+    "doums/espresso.nvim",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "xero/miasma.nvim",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "qaptoR-nvim/chocolatier.nvim",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "gustavoprietop/doom-themes.nvim",
+    priority = 1000,
+    lazy = false,
+  },
+  -- Kanagawa theme
+    {
+    "AlessandroYorba/Alduin",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "neanias/everforest-nvim",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "aktersnurra/no-clown-fiesta.nvim",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    priority = 1000,
+    lazy = false
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- Bamboo theme
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- Lush (required for Ares)
+  {
+    "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1001,  -- Load before Ares
+  },
+  -- Ares theme
+  {
+    'ab-dx/ares.nvim',
+    dependencies = { "rktjmp/lush.nvim" },
+    lazy = false,
+    priority = 1000,
+  },
+  -- Gruvbox Baby theme
+  {
+    'luisiacc/gruvbox-baby',
+    branch = 'main',
+    lazy = false,
+    priority = 1000,
+  },
+  -- Gruvbox Material theme
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+  }
+}
---
+++ lua/plugins/themery.lua
@@ -0,0 +1,140 @@
+return {
+  "zaldih/themery.nvim",
+  lazy = false,
+  config = function()
+    require("themery").setup({
+      themes = {
+        {
+          name = "posterpole",
+          colorscheme = "posterpole",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "posterterm",
+          colorscheme = "posterpole-term",
+          before = [[
+            vim.opt.background = "dark"
+            require("notify").setup({
+              background_colour = "#000000",
+            })
+          ]]
+        },
+        {
+          name = "rakis",
+          colorscheme = "rakis",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "ubaryd",
+          colorscheme = "ubaryd"
+        },
+        {
+          name = "kanagawa wave",
+          colorscheme = "kanagawa-wave"
+        },
+        {
+          name = "kanagawa dragon",
+          colorscheme = "kanagawa-dragon"
+        },
+        {
+          name = "bamboo",
+          colorscheme = "bamboo"
+        },
+        {
+          name = "ares",
+          colorscheme = "ares"
+        },
+        {
+          name = "gruvbox baby",
+          colorscheme = "gruvbox-baby"
+        },
+        {
+          name = "gruvbox material",
+          colorscheme = "gruvbox-material",
+          before = [[
+            vim.g.gruvbox_material_enable_italic = true
+          ]]
+        },
+        {
+          name = "alduin",
+          colorscheme = "alduin"
+        },
+        {
+          name = "oxocarbon",
+          colorscheme = "oxocarbon",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "no clown fiesta",
+          colorscheme = "no-clown-fiesta"
+        },
+        {
+          name = "everforest",
+          colorscheme = "everforest",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "doom miramare",
+          colorscheme = "doom-miramare",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "miasma",
+          colorscheme = "miasma",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "chocolatier",
+          colorscheme = "chocolatier",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "coffee",
+          colorscheme = "coffee",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "espresso",
+          colorscheme = "espresso",
+          before = [[
+            vim.opt.background = "dark"
+          ]]
+        },
+        {
+          name = "kanagawa lotus",
+          colorscheme = "kanagawa-lotus"
+        },
+        {
+          name = "doom gruvbox light",
+          colorscheme = "doom-gruvbox",
+          before = [[
+            vim.opt.background = "light"
+          ]]
+        },
+        {
+          name = "rakis light",
+          colorscheme = "rakis",
+          before = [[
+            vim.opt.background = "light"
+          ]]
+        },
+      }
+    })
+  end
+}
